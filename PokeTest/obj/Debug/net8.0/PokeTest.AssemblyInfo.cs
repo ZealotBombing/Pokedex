66728e39dd3d4e795aec4e0629b539c4219376ad
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-05-07 00:15:33 -0400

    test list from imp

--- PokeTest/obj/Debug/net8.0/PokeTest.AssemblyInfo.cs
+++ PokeTest/obj/Debug/net8.0/PokeTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PokeTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0d372f8f11d281b2ad3632cf59db9503408daed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797c7a26bab91cd05a820698727bb74594e38a78")]
 [assembly: System.Reflection.AssemblyProductAttribute("PokeTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PokeTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
