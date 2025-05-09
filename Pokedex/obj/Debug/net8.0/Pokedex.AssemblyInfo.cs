7aa1791e0c49433beab4f087a93b001a6610c525
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-05-09 01:27:09 -0400

    apply grid to pokemon list

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f88780d52b3166e35413cdfea31d91896d40a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11eb89de61186526224e12ac748c4c5b6fd27824")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
