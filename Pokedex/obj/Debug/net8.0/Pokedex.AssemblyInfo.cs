6de9a6644a9fd5d7263c6e37eaf5f60470f35412
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-05-28 23:16:33 -0400

    IDK

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f5f27ce886c023a33c8ef32dc82b6677cbc1c47")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0078d00696e060b342e478d71b69ed3dc146b120")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
