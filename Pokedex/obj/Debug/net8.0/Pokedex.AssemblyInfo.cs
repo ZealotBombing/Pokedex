fe990f2eca2b1e1ab63cdc3bcd4f17e146e8c1c9
Cristian Fuentes <cfuentesguerrero333@gmail.com>
2025-05-31 00:42:06 -0400

    Including pokemon script

--- Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
+++ Pokedex/obj/Debug/net8.0/Pokedex.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4838632f290b1192b469399c1658325bcea1b589")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd5ee57a2d6f7979426b8698cac1acc6e2502d92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pokedex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
